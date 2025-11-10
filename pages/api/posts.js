ea99349bf30a9121796c9a4648cc5ca87076120e
1lkin13 <ilkin.emiraslanov1453@gmail.com>
2025-11-10 15:20:05 +0400

    cors

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -20,6 +20,14 @@ const readPosts = () => {
 };
 
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -27,5 +35,3 @@ export default function handler(req, res) {
   const posts = readPosts();
   return res.status(200).json(posts);
 }
-
-
