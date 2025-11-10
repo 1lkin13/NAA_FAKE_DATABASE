ae551c4524abd73243ab4fc1110609bcc5bf5a26
1lkin13 <ilkin.emiraslanov1453@gmail.com>
2025-11-10 15:24:52 +0400

    fix

--- pages/api/ut-upload.js
+++ pages/api/ut-upload.js
@@ -1,10 +1,11 @@
 export const config = { api: { bodyParser: false } };
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     return res.status(204).end();
   }
 
